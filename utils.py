80186a1224e9692520eed864cc91345a8f5438c9
junsukha <junsukha@gmail.com>
2026-01-14 22:50:29 +0900

    apply ruff

--- utils.py
+++ utils.py
@@ -7,22 +7,17 @@ import ssl
     
 from imap_tools import MailBox, AND
 from datetime import datetime, timedelta
-import smtplib
 from email.mime.text import MIMEText
 
 import os
-import time
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 
 # deprecated
 def send_naver_report(user_id, user_pw, recipient_user_id, report_text):
